9fa4a2321206609a3e141e068c56f430fc904567
TyShimizu1 <tjshimiz@calpoly.edu>
2026-05-28 20:49:40 -0700

    commit

--- bst.py
+++ bst.py
@@ -3,3 +3,94 @@ import unittest
 from typing import *
 from dataclasses import dataclass
 sys.setrecursionlimit(10**9)
+
+
+BST: TypeAlias= Union["BSTNode", None]
+
+@dataclass
+class BSTNode:
+    val: Any 
+    left: BST 
+    right: BST 
+
+@dataclass(frozen=True)
+class BSTWithComesBefore:
+    comes_before:Callable[[Any,Any],bool]
+    tree: BST
+
+
+
+def same_value(comes_before: Callable[[Any, Any], bool], val1: Any, val2: Any) -> bool:
+    return not comes_before(val1, val2) and not comes_before(val2, val1)
+
+#checks if "val" is in bst 
+def lookup(bst: BSTWithComesBefore, val:Any)->bool:
+    
+    def helper(tree:BST, val:Any)->bool:
+        if tree is None:
+            return False
+        
+        if same_value(bst.comes_before, val, tree.val):
+            return True 
+        elif bst.comes_before(tree.val, val):
+             return helper(tree.right, val)
+        else: 
+            return helper(tree.left, val)
+    
+    return helper(bst.tree, val)            
+
+#inserts "val" into bst 
+def insert(bst: BSTWithComesBefore, val:Any)->BSTWithComesBefore:
+    def helper(tree:BST, val:Any)->BST:
+        if tree is None:
+            return BSTNode(val, None, None)
+        if same_value(bst.comes_before, val, tree.val):
+            return tree 
+        elif bst.comes_before(tree.val, val):
+            tree.right= helper(tree.right, val)
+            return tree 
+        else:
+            tree.left= helper(tree.left, val)
+            return tree 
+    new_tree= helper(bst.tree, val)
+    return BSTWithComesBefore(bst.comes_before, new_tree)
+        
+#helper function that finds min value in "tree"
+def find_min(tree:BST)->Any:
+    if tree is None:
+        return None 
+    
+    if tree.left is None:
+        return tree.val
+    return find_min(tree.left)
+
+#Deletes "val" in bst 
+def delete(bst: BSTWithComesBefore, val:Any)->BSTWithComesBefore:
+    def helper(tree:BST, val:Any)->BST:
+        if tree is None:
+            return None 
+        if bst.comes_before(val, tree.val):
+            tree.left = helper(tree.left, val)
+            return tree 
+        
+        elif bst.comes_before(tree.val, val): 
+            tree.right= helper(tree.right, val)
+            return tree 
+        else: 
+            if tree.left is None:
+                return tree.right 
+            
+            if tree.right is None: 
+                return tree.left
+            
+            new_val: Any= find_min(tree.right)
+            tree.val= new_val
+            tree.right= helper(tree.right, new_val)
+            return tree 
+    new_tree= helper(bst.tree, val)
+    return BSTWithComesBefore(bst.comes_before, new_tree)
+
+
+
+
+
--- bst_tests.py
+++ bst_tests.py
@@ -4,8 +4,66 @@ from typing import *
 from dataclasses import dataclass
 sys.setrecursionlimit(10**9)
 from bst import *
-class BSTTests(unittest.TestCase):
-def test_example(self):
-pass
-if (__name__ == '__main__'):
-unittest.main()
+
+
+@dataclass
+class Point2D:
+ x: float
+ y: float   
+
+#gets alphebetical order 
+def alphabetical(str1: str, str2: str) -> bool:
+    return str1 < str2
+
+#gets the distance between points 
+def point_distance(p1: Point2D, p2: Point2D) -> bool:
+    return p1.x**2 + p1.y**2 < p2.x**2 + p2.y**2
+
+#
+def reverse_ints(n1: int, n2: int) -> bool:
+    return n1 > n2
+
+
+class Tests(unittest.TestCase):
+
+    def test_alphabetical_strings(self):
+        bst:BSTWithComesBefore = BSTWithComesBefore(alphabetical, None)
+
+        bst = insert(bst, "b")
+        bst = insert(bst, "a")
+        bst = insert(bst, "c")
+
+        self.assertEqual(bst.tree.val, "b")
+        self.assertEqual(bst.tree.left.val, "a")
+        self.assertEqual(bst.tree.right.val, "c")
+
+    def test_points_by_distance(self):
+        bst = BSTWithComesBefore(point_distance, None)
+
+        point1 = Point2D(0, 1)
+        point2 = Point2D(3, 4)
+        point3 = Point2D(6, 8)
+
+        bst = insert(bst, point2)
+        bst = insert(bst, point1)
+        bst = insert(bst, point3)
+
+        self.assertEqual(bst.tree.val, point2)
+        self.assertEqual(bst.tree.left.val, point1)
+        self.assertEqual(bst.tree.right.val, point3)
+
+    def test_reverse_order_integers(self):
+        bst = BSTWithComesBefore(reverse_ints, None)
+
+        bst = insert(bst, 1)
+        bst = insert(bst, 2)
+        bst = insert(bst, 0)
+
+        self.assertEqual(bst.tree.val, 1)
+        self.assertEqual(bst.tree.left.val, 2)
+        self.assertEqual(bst.tree.right.val, 0)
+
+if __name__ == "__main__":
+    unittest.main()
+
+
